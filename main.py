b9a631409c0a65b2cbe8ba1389b99a8a90559d71
Pedro Fernandez Sanchez <72625216+PedroFedz@users.noreply.github.com>
2023-04-20 19:46:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,118 @@
 import random
 
+import self as self
+
+
+class Requirement:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.valores = [1] * num_requisitos
+        self.valorFinal = 0
+        self.getValorFinal()
+        self.peso = 1
+
+    def getValorFinal(self):
+        mid = 0
+        for val in self.valores:
+            mid += val
+        self.valorFinal = mid / len(self.valores)
+
+
+    def __str__(self):
+        return f"{self.nombre} - Valor final: {self.valorFinal}"
+
+    def __lt__(self, other):
+        return self.valorFinal > other.valorFinal
+
+
+
+class StakeHolder:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.valores = [1] * num_stakeholders
+        self.valorFinal = 0
+        self.getValorFinal()
+
+    # @property
+    # def valores(self):
+    #     return self.valores
+    #
+    #
+    # @valores.setter
+    # def valores(self, index, valor):
+    #     self.valores[index] = valor
+    #     self.getValorFinal()
+
+    def getValorFinal(self):
+        mid = 0
+        for val in self.valores:
+            mid += val
+        self.valorFinal = mid / len(self.valores)
+
+    def __str__(self):
+        return f"{self.nombre} - Valor final: {self.valorFinal}"
+
+    def __lt__(self, other):
+        return self.valorFinal > other.valorFinal
+
+
+
+
 # Obtener el numero de stakeholders y requisitos
 num_stakeholders = int(input("Introduce el numero de stakeholders: "))
 num_requisitos = int(input("Introduce el numero de requisitos: "))
 
 # Crear las listas vacias para stakeholders y requisitos
-stakeholders = [[] for _ in range(num_stakeholders)]
-requisitos = [[] for _ in range(num_requisitos)]
+stakeholders = [StakeHolder("S" + str(i+1)) for i in range(num_stakeholders)]
+requisitos = [Requirement("R" + str(i+1)) for i in range(num_requisitos)]
+ordenStake = []
+
 
 # Llenar las listas de stakeholders y requisitos con valores aleatorios
 for i in range(num_stakeholders):
     for j in range(num_stakeholders):
         if i == j:
-            stakeholders[i].append(None)
+            print("pasa")
+            stakeholders[i].valores[j] = 0
+            #stakeholders[i].append(None)
         else:
-            stakeholders[i].append(random.randint(1, 10))
-    for j in range(num_requisitos):
-        stakeholders[i].append(random.randint(1, 10))
+            stakeholders[i].valores[j] = random.randint(1, 10)
+            #stakeholders[i].append(random.randint(1, 1))
 
 for i in range(num_requisitos):
     for j in range(num_stakeholders):
-        requisitos[i].append(random.randint(1, 10))
+        requisitos[i].valores[j] = (random.randint(1, 10))
+
+#En funcion de la importancia del stakeholder aumentara el valor del requisito
+
+
+def valueStakeHolder():
+    #Se calcula el valor de cada stakeholder
+    for i in range(num_stakeholders):
+        stakeholders[i].getValorFinal()
+
+
+    #Ordena los stakeholders en funcion de su valor
+    ordenStake = sorted(stakeholders)
+    for i in range(num_stakeholders):
+        #A partir del nombre obtenemos el index del objeto al que pertenece
+        index = int(ordenStake[i].nombre[1]) - 1
+        valor = stakeholders[index].valorFinal
+        for j in range(num_requisitos):
+            #Para dar mas importancia a los stakeholders mejores valorados sumamos el valor final
+            #Obteniendo mayor valor los requisitos determinados por este
+            requisitos[j].valores[int(index)] = valor + requisitos[j].valores[int(index)]
+
+def valueRequeriment():
+    #Se calcula el valor de cada stakeholder
+    for i in range(num_requisitos):
+        requisitos[i].getValorFinal()
+
+    #Ordena los stakeholders en funcion de su valor
+    ordenReq = sorted(requisitos)
+    for i in range(num_requisitos):
+        print(ordenReq[i])
+    return ordenReq
 
 # Ciclo principal del programa
 while True:
@@ -31,6 +123,7 @@ while True:
     print("3. Valorar un stakeholder")
     print("4. Valorar un requisito")
     print("5. Salir")
+    print("6. Algoritmo")
 
     opcion = int(input("Introduce el numero de la opcion que deseas: "))
 
@@ -38,13 +131,14 @@ while True:
         # Mostrar los valores actuales de stakeholders
         print("Valoracion actual de los stakeholders:")
         for i in range(num_stakeholders):
-            print(f"Stakeholder {i+1}: {stakeholders[i]}")
+            print(stakeholders[i].valores)
+            #print(f"Stakeholder {i+1}: {stakeholders[i].valores[j]}")
 
     elif opcion == 2:
         # Mostrar los valores actuales de requisitos
         print("Valoracion actual de los requisitos:")
         for i in range(num_requisitos):
-            print(f"Requisito {i+1}: {requisitos[i]}")
+            print(f"Requisito {i+1}: {requisitos[i].valores}")
 
     elif opcion == 3:
         # Valorar un stakeholder
@@ -62,7 +156,8 @@ while True:
         valoracion = int(input("Introduce la valoracion del stakeholder: "))
 
         # Actualizar la valoracion del stakeholder
-        stakeholders[stakeholder-1][stakeholder_valorado-1] = valoracion
+        stakeholders[stakeholder-1].valores[stakeholder_valorado-1] = valoracion
+        #stakeholders[stakeholder-1][stakeholder_valorado-1] = valoracion
 
     elif opcion == 4:
         # Valorar un requisito
@@ -79,13 +174,34 @@ while True:
         valoracion = int(input("Introduce la valoracion del requisito: "))
 
         # Actualizar la valoracion del requisito
-
-        requisitos[requisito-1][stakeholder-1] = valoracion
+        requisitos[requisito-1].valores[stakeholder-1] = valoracion
 
     elif opcion == 5:
         # Salir del programa
         print("¡Hasta luego!")
         break
 
+
+    elif opcion == 6:
+        #Algoritmo mochila
+        total_capacity = int(input("Introduce el peso deseado: "))
+
+        # Obtencion del valoraciones de stakeholder
+        valueStakeHolder()
+        # Ordenacion de valoraciones de requisitos
+        ordenReq = valueRequeriment()
+
+        selected_items = []
+        capacity = 0
+        for item in ordenReq:
+            if capacity + item.peso <= total_capacity:
+                selected_items.append(item.nombre)
+                capacity += item.peso
+
+        print(selected_items)
+        break
+
     else:
         print("Opcion invalida. Por favor, intenta de nuevo.")
+
+
